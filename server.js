aa32a8f2c81f67fb8b8368bdbb956332b74bc134
Dallasss7 <mr.dalsummers@gmail.com>
2014-12-03 19:05:56 -0800

    dependencies changed and added

--- server.js
+++ server.js
@@ -4,49 +4,41 @@ var config = require('./public/config');
 
 var Router = require("routes-router");
 
-var sendHtml = require("send-data/html");
-
 var nominees = require('./public/nomineeYears.js')
-
-var fs = require("fs");
 
 var st = require('st');
 
 var db = require("orchestrate")(config.dbKey);
 
 var router = Router();
+			
 
-router.addRoute("/api", {
+router.addRoute("/year", function(){
 
-	GET: function(req, res, opts){
-			console.log("getting. . . ");
-			db.get('nominees', results.values[0])
-.then(function (res) {
-  console.log(res.body);
+
+	// GET: function(req, res, opts){
+	// 		console.log("getting. . . ");
+	// 		db.get('nominees', "results")
+	// 		.then(function (res) {
+ //  				console.log(res.body);
+	// 		})
+	// 		.fail(function (err) {});
+			
+	// 		res.end("Got it!")
+	// },
+
+	// POST: function(req, res, opts){
+	// 		console.log("putting. . . ");
+	// 		console.log(JSON.stringify(opts));
+	// 		res.end("It's posted!")
+	// }
 })
-.fail(function (err) {});
-			console.log(JSON.stringify(opts));
-			res.end("Got it!")
-	},
-
-	POST: function(req, res, opts){
-			console.log("putting. . . ");
-			console.log(JSON.stringify(opts));
-			res.end("It's posted!")
-	}
-})
 
 
-router.addRoute("/", function(req,res){
-
-	fs.readFile('./public/dropdown.html', {encoding:'utf8'}, function (err, data){
-
-		if(err) console.log("ERROR :" + err)
-
-	sendHtml(req,res,data)
-	});
-
-});
+// router.addRoute("/year/:name", function (req,res,opts) {
+//     var name = opts.params.name;
+//     res.end("hello, "+name+"!")
+// })
 
 router.addRoute("/*", st({
 
