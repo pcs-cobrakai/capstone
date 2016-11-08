bf44e27c4b75083a45f6869b69d567121436bd97
Dallasss7 <mr.dalsummers@gmail.com>
2016-11-07 19:41:28 -0800

    fixed the responsive issue with the iframe

--- public/js/views.js
+++ public/js/views.js
@@ -138,14 +138,95 @@ atna.views.movieView = Backbone.View.extend({
 	
 	render: function() {
 		this.$el.html(this.template(this.movieInfo)).appendTo('#results-list');
-		
+		var iframeDiv = $('.iframe_div');
 		//fancybox
 		$('.view-trailer').fancybox({
 			padding: 3,
-			aspectRatio: true
+			aspectRatio: true,
+			afterShow:function(){
+				var fancyWidth = $('.fancybox-inner').width();
+				var fancyHeight = $('.fancybox-inner').height();
+				var iframe = $('#fancy_iframe');
+				iframe.css({ 'width': fancyWidth + 'px'});
+				iframe.css({ 'height': fancyHeight + 'px'});
+			}
+			
 		});
+
+		// this.$el.html(this.template(this.movieInfo)).appendTo('#results-list');
+		
+		// //fancybox
+		// $('.view-trailer').fancybox({
+		// 	padding: 3,
+		// 	aspectRatio: true
+		// });
+
+
+
 
 
 	}
 	
 });
+
+
+
+$(window).load(function(){
+	
+	
+
+
+
+	// var windowHeight = $(window).height(), winWidth = $(window).width();
+
+	
+	// if(winWidth>768)
+	// {
+		
+	// }
+
+	// if(winWidth<768)
+	// {
+		//width="640" height="390"  from the index page for the iframe
+		
+	
+	// }
+
+	// else if(winWidth>1850)
+	// {
+		
+	// }
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
